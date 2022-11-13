8dad35cb2ea7a30786c759928a15ba49141e9be9
Kyaw Zay Ya Lin Tun <kyaw.codes@gmail.com>
2022-11-13 10:09:25 +0630

    chore: initial project

---
+++ BahHatHatSocial/BahHatHatSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  BahHatHatSocialApp.swift
+//  BahHatHatSocial
+//
+//  Created by Kyaw Zay Ya Lin Tun on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct BahHatHatSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
